37db58d5fd6cd9831a75903ed185a8e4186cffd7
Click Soluciones Digital <clicksoluciones.digital@gmail.com>
2026-09-12 15:34:53 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,11 @@
 import type { NextConfig } from "next";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const githubPagesBasePath =
-  process.env.GITHUB_PAGES === "true" && repoName ? `/${repoName}` : "";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+
   images: {
     unoptimized: true,
-  },
-  basePath: githubPagesBasePath || undefined,
-  assetPrefix: githubPagesBasePath || undefined,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: githubPagesBasePath,
   },
 };
 
